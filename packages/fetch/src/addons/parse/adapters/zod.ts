c8121a330348e90703dea4cbe2799fc69187c3d8
Damir Modyarov <otomir@yandex.ru>
2025-03-08 18:59:40 +0300

    feat(fetch): add safeParse function

--- packages/fetch/src/addons/parse/adapters/zod.ts
+++ packages/fetch/src/addons/parse/adapters/zod.ts
@@ -1,9 +1,10 @@
-import type { ParseParams, z } from 'zod'
+import type { ParseParams, SafeParseReturnType, z } from 'zod'
 
 import type { FfetchParser, FfetchTypeProvider } from '../_types.js'
 
 export interface ZodTypeProvider extends FfetchTypeProvider {
     readonly parsed: this['schema'] extends z.ZodTypeAny ? z.infer<this['schema']> : never
+    readonly safeParsed: this['schema'] extends z.ZodTypeAny ? SafeParseReturnType<this['schema'], z.infer<this['schema']>> : never
 }
 
 export function ffetchZodAdapter({ async, ...rest }: Partial<ParseParams> = {}): FfetchParser<ZodTypeProvider> {
@@ -15,9 +16,17 @@ export function ffetchZodAdapter({ async, ...rest }: Partial<ParseParams> = {}):
         : function (schema: unknown, value: unknown): unknown {
             return (schema as z.ZodTypeAny).parse(value, rest)
         }
+    const safeParse = async
+        ? async function (schema: unknown, value: unknown): Promise<z.SafeParseReturnType<unknown, unknown>> {
+            return (schema as z.ZodTypeAny).safeParseAsync(value, rest)
+        }
+        : function (schema: unknown, value: unknown): z.SafeParseReturnType<unknown, unknown> {
+            return (schema as z.ZodTypeAny).safeParse(value, rest)
+        }
 
     return {
         _provider,
         parse,
+        safeParse,
     }
 }
